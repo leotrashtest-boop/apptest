d53c46abfc2250df85a6b363543277dce9401d6a
leotrashtest-boop <leotrashtest@gmail.com>
2026-09-19 14:32:24 +0200

    Add files via upload

---
+++ sw.js.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'random-app-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Installation du Service Worker et mise en cache des fichiers
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Interception des requêtes : réponse depuis le cache si disponible
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
